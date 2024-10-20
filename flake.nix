7222d0e5049de9fcffdbb543d863279868791a16
brokenpike <brokenpike@garmr.org>
2024-10-20 13:31:01 +0200

    specialArgs output

--- flake.nix
+++ flake.nix
@@ -1,9 +1,22 @@
 {
   description = "gnome framework nixos root flake";
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-  outputs = { self, nixpkgs }: {
-    # replace 'joes-desktop' with your hostname here.
+  # inputs are an attribute set
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = github:nix-community/home-manager;
+    };
+
+  };
+  #outputs is a function of one argument that takes an attribute set of all the realized inputs
+  outputs = { self, nixpkgs,...}@inputs: {
+    # replace 'joes-desktop' with your networking.hostname here.
     nixosConfigurations.framework = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
       modules = [ ./configuration.nix ];
     };
   };
