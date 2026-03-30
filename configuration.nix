981e2ed16e1ff99426c4c4bd3b1ec45f26246a36
brokenpike <brokenpike@garmr.org>
2026-03-30 10:17:33 +0200

    flake update

--- configuration.nix
+++ configuration.nix
@@ -68,16 +68,16 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  #services.displayManager.gdm.enable = true;
-  #services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Enable the COSMIC login manager
-  services.displayManager.cosmic-greeter.enable = true;
+  #services.displayManager.cosmic-greeter.enable = true;
 
   # Enable the COSMIC desktop environment
-  services.desktopManager.cosmic.enable = true;
+  #services.desktopManager.cosmic.enable = true;
 
-  services.system76-scheduler.enable = true;
+  #services.system76-scheduler.enable = true;
   # affects clipboard behaviour
   #environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
   # Configure keymap in X11i
