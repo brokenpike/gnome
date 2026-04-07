81fc5d0e4e53aa7bca2ddd2fc7a3ef0edde55541
brokenpike <brokenpike@garmr.org>
2026-04-07 10:28:52 +0200

    flake update

--- configuration.nix
+++ configuration.nix
@@ -70,7 +70,10 @@
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
-
+  services.desktopManager.gnome.extraGSettingsOverrides = ''
+    [org.gnome.mutter]
+    experimental-features=['scale-monitor-framebuffer', 'xwayland-native-scaling']
+  '';
   # Enable the COSMIC login manager
   #services.displayManager.cosmic-greeter.enable = true;
 
