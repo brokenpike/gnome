4eefaf60614201c0d9a44ddc11b269d9eede1427
brokenpike <brokenpike@garmr.org>
2026-05-27 07:54:41 +0200

    updated stable to 26.06 running gnome 50

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     # nvf = {
     #   url = "github:notashelf/nvf";
